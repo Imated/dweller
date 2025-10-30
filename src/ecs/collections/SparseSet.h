dce7dd64d9009be11f216ce87144cd660fd60a81
Imated <lineborder122@gmail.com>
2025-10-30 15:08:06 -0400

    Added pagination

--- src/ecs/collections/SparseSet.h
+++ src/ecs/collections/SparseSet.h
@@ -24,8 +24,7 @@ namespace ecs::collections {
     template<typename T, size_t Capacity = 1024>
     class SparseSet : ISpareSet {
     public:
-        SparseSet(): sparseArray() {
-            sparseArray.fill(tombstone);
+        SparseSet(): sparsePages() {
             denseArray.reserve(Capacity);
             denseToEntityArray.reserve(Capacity);
         }
@@ -33,14 +32,21 @@ namespace ecs::collections {
         bool contains(size_t id) override {
             if (id >= Capacity)
                 return false;
-            auto index = sparseArray[id];
-            return index != tombstone && index < denseArray.size() && denseToEntityArray[index] == id;
+            const auto index = getDenseIndex(id);
+            return !denseArray.empty() && index != tombstone;
         }
 
         bool add(size_t id, const T& value) {
             if (id > Capacity || contains(id))
                 return false;
-            sparseArray[id] = denseArray.size();
+            size_t index = getDenseIndex(id);
+            if (index != tombstone) {
+                denseArray[index] = value;
+                denseToEntityArray[index] = id;
+                return true;
+            }
+
+            setDenseIndex(id, denseArray.size());
             denseArray.push_back(value);
             denseToEntityArray.push_back(id);
             return true;
@@ -49,32 +55,56 @@ namespace ecs::collections {
         bool remove(size_t id) override {
             if (id > Capacity || !contains(id))
                 return false;
-            size_t index = sparseArray[id];
-            size_t lastIndex = denseArray.size() - 1;
-            std::swap(denseArray[index], denseArray[lastIndex]);
-            std::swap(denseToEntityArray[index], denseToEntityArray[lastIndex]);
-            sparseArray[denseToEntityArray[index]] = index;
+            size_t index = getDenseIndex(id);
+
+            setDenseIndex(denseToEntityArray.back(), index);
+            setDenseIndex(id, tombstone);
+
+            std::swap(denseArray[index], denseArray.back());
+            std::swap(denseToEntityArray[index], denseToEntityArray.back());
+
             denseArray.pop_back();
             denseToEntityArray.pop_back();
-            sparseArray[id] = tombstone;
             return true;
         }
 
         const T* get(size_t id) {
             ECS_ASSERT(contains(id), "Couldn't find id of " << id << " in sparse set of type " << typeid(T).name() << "!");
-            return denseArray[sparseArray[id]];
+            size_t index = getDenseIndex(id);
+            return denseArray[index];
         }
 
         const T& get(size_t id) const {
             ECS_ASSERT(contains(id), "Couldn't find id of " << id << " in sparse set of type " << typeid(T).name() << "!");
-            return denseArray[sparseArray[id]];
+            size_t index = getDenseIndex(id);
+            return denseArray[index];
         }
 
     private:
-        static constexpr size_t tombstone = std::numeric_limits<size_t>::max();;
+        size_t getDenseIndex(size_t id) {
+            int page = id / Capacity;
+            int index = page % Capacity;
+            if (page < sparsePages.size() && index < Capacity)
+                return sparsePages[page][index];
+            return tombstone;
+        }
+
+        void setDenseIndex(size_t id, size_t index) {
+            int page = id / Capacity;
+            int pageIndex = page % Capacity;
+
+            if (page >= sparsePages.size()) {
+                sparsePages.resize(page + 1);
+                sparsePages[page].fill(tombstone);
+            }
+
+            sparsePages[page][pageIndex] = index;
+        }
+
+        static constexpr size_t tombstone = std::numeric_limits<size_t>::max();
         using sparse = std::array<size_t, Capacity>;
 
-        sparse sparseArray;
+        std::vector<sparse> sparsePages;
         std::vector<T> denseArray;
         std::vector<size_t> denseToEntityArray;
     };
